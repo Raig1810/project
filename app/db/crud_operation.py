5fe3cbc6524fed041b183c3137ed208ee594f0ef
RAIG1810 <gavrilov.aleksander2000@gmail.com>
2026-06-27 15:17:37 +0300

    Возможность работы с JSON

--- app/db/crud_operation.py
+++ app/db/crud_operation.py
@@ -1,13 +1,35 @@
+import json
 from app.schemas import Student
 
 class CRUD():
-    def __init__(self):
+    def __init__(self, db_path: str = "data/students.json"):
+        self.db_path = db_path
         self.db = {}
         self.current_id = 1
+        self._load_data()
 
-    def create(self, name: str, group: str, average_score: float):
+    def _load_data(self):        
+        try:
+            with open(self.db_path, "r", encoding = "utf-8") as file:
+                data = json.load(file)
+                for k, v in data.items():
+                    self.db[int(k)] = Student(**v)
+                if self.db:
+                    self.current_id = max(self.db.keys()) + 1
+        except FileNotFoundError:
+            pass
+                    
+    def _save_data(self):
+        try:
+            data_to_save = {str(k): v.__dict__ for k, v in self.db.items()}
+            with open(self.db_path, "w", encoding = "utf-8") as file:
+                json.dump(data_to_save, file, ensure_ascii = False, indent = 4)
+        except Exception as error:
+            print("Ошибка при сохранении файла")
+
+    def create(self, name: str, group: str, average_score: float) -> Student:
         if not name or name.strip() == "":
-            raise ValueError("Ошибка: имя цне должно быть пустым")
+            raise ValueError("Ошибка: имя не должно быть пустым")
         if average_score < 0 or average_score > 100:
             raise ValueError("Ошибка: средний балл должен находится в диапазоне от 0 до 100")
         
@@ -17,12 +39,13 @@ class CRUD():
             group = group,
             average_score = average_score
         )
-        self.db[self.currnet_id] = new_student
+        self.db[self.current_id] = new_student
         self.current_id += 1
 
+        self._save_data()
         return new_student
     
-    def read(self, student_id: int):
+    def read(self, student_id: int) -> Student:
         if student_id not in self.db:
             raise ValueError(f"Ошибка: Студент с ID {student_id} не найден")
         student = self.db[student_id]
@@ -31,7 +54,7 @@ class CRUD():
         
         return student
     
-    def update(self, student_id: int, new_name: str = None, new_score: float = None,):
+    def update(self, student_id: int, new_name: str = None, new_score: float = None,) -> Student:
         student = self.read(student_id)
 
         if new_name is not None:
@@ -41,12 +64,14 @@ class CRUD():
 
         if new_score is not None:
             if new_score < 0 or new_score > 100:
-                raise ValueError("Ошибка: новый балл должен быть находиться в диапазоне от 0 до 100")
+                raise ValueError("Ошибка: новый балл должен находиться в диапазоне от 0 до 100")
             student.average_score = new_score
             
+        self._save_data()
         return student
     
-    def delete(self, student_id: int):
+    def delete(self, student_id: int) -> str:
         student = self.read(student_id)
         student.is_active = False
+        self._save_data()
         return f"Студент {student.name} удален"
